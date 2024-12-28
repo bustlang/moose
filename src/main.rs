a9a6c6d80e41055a34e5e9e8ec2e8e257669e4de
Hazle <xendyex@tutanota.com>
2024-12-28 11:05:42 +0000

    Fix: "Bust Package" -> "Reef"

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Moose - Package Manager For Bust");
-    println!("-i - Install A Bust Package");
-    println!("-r - Remove A Bust Package");
+    println!("-i - Install A Reef (package)");
+    println!("-r - Remove A Reef (package)");
     println!("-run - Compile And Run The Project");
     println!("---> THIS DOESN'T WORK RIGHT NOW, THERE IS NO OFFICIAL WAY TO INSTALL PACKAGES RIGHT NOW. <---");
 }
