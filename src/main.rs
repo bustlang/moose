501e0f359780d02430b192f4567abada5f588fd3
Anjo2807 <dmytro.lopatyn@gmail.com>
2024-12-28 11:50:04 +0100

    Added help

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     println!("Moose - Package Manager For Bust");
-    println!("---> THIS DOESN'T WORK RIGHT NOW, THERE IS NO OFFICIAL WAY TO INSTALL PACKAGES RIGHT NOW. <---")
+    println!("-i - Install A Bust Package");
+    println!("-r - Remove A Bust Package");
+    println!("-run - Compile And Run The Project");
+    println!("---> THIS DOESN'T WORK RIGHT NOW, THERE IS NO OFFICIAL WAY TO INSTALL PACKAGES RIGHT NOW. <---");
 }
